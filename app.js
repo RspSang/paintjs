cbe5ad08aeea8c94d1255fe7cfbbfda382ecbb5c
HealMon <53738172+HealMon@users.noreply.github.com>
2021-06-14 22:13:41 +0900

    add color handler

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 800;
 canvas.height = 800;
@@ -29,6 +30,11 @@ function onMouseMove(event) {
   }
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -36,3 +42,6 @@ if (canvas) {
   canvas.addEventListener("mouseleave", stopPainting);
 }
 
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
